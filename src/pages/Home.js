71dd9eb6910604b183223a457f343d1e2543d992
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-12 23:22:39 +0700

    links to products page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
   return (
-    <div>
+    <>
       <h1>Home Page</h1>
-    </div>
+      <p>
+        Go to <Link to={"/products"}>Products Page</Link>
+      </p>
+    </>
   );
 }
