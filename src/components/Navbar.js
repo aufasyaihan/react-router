95a23712c85b7fa30285847185663ca695d79224
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-15 13:01:38 +0700

    added active route style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,33 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
+  const underline = (
+    <span className="block h-0.5 bg-amber-300 max-w-0 group-hover:max-w-full transition-all duration-300 ease-in"></span>
+  );
   return (
     <nav className="p-4 text-amber-300">
       <ul className="flex gap-2 w-full justify-center">
         <li>
-          <Link to={"/"}>Home</Link>
+          <NavLink
+            to={"/"}
+            className={({ isActive }) =>
+              isActive ? "border-b-[2.5px] border-b-amber-300 pb-[2px]" : "group"
+            }
+            end // to tell that the link is ends with "/"
+          >
+            Home
+            {underline}
+          </NavLink>
         </li>
         <li>
-          <Link to={"/products"}>Products</Link>
+          <NavLink
+            to={"/products"}
+            className={({ isActive }) =>
+              isActive ? "border-b-[2.5px] border-b-amber-300 pb-[2px]" : "group"
+            }
+          >
+            Products {underline}
+          </NavLink>
         </li>
       </ul>
     </nav>
