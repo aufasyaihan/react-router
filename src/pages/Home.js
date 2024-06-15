ebb8b3ffe737204c2fbce4f07903c2a71ccb9f1f
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-15 13:07:21 +0700

    navigate programmatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate('/products') // not recommended, use Link instead
+  }
   return (
     <>
       <h1 className="text-3xl">Home Page</h1>
       <p>
         Go to <Link to={"/products"} className="text-amber-400 underline underline-offset-2">Products Page</Link>
       </p>
+      <button onClick={navigateHandler} className="px-4 py-2 bg-amber-500 mt-5 rounded-md hover:bg-amber-600">Products</button>
     </>
   );
 }
